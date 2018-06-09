be467da9fa15a6c15c941bedbaacecf4edeaf3f9
Sung Kim <sungmkim80@gmail.com>
2018-06-09 17:17:46 -0400

    Created routes

---
+++ src/components/PasswordForget.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PasswordForget = () => (
+  <div>
+    <h1>Password Forget Page</h1>
+  </div>
+);
+
+export default PasswordForget;
---
+++ src/components/SignOut.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignOut = () => (
+  <div>
+    <h1>Sign Out Page</h1>
+  </div>
+);
+
+export default SignOut;
